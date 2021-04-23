bc3246feafa463d9f1866b5e2ccddd537d840a58
fasalismail98 <82331724+fasalismail98@users.noreply.github.com>
2021-04-23 20:35:29 +0530

    Add files via upload

---
+++ qpapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class QpappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'qpapp'
---
+++ qpapp/form.py
@@ -0,0 +1,48 @@
+from .models import *
+from django import forms
+
+# class subjects(forms.ModelForm):
+#     class Meta:
+#         model=subject
+#         fields=[
+#             'python',
+#             'mathematics',
+#             'algorithm',
+#
+#
+#         ]
+#
+#
+# class pychapters(forms.ModelForm):
+#     class Meta:
+#         model =pychapter
+#         fields = [
+#             'pycha1',
+#             'pycha2',
+#             'pycha3',
+#             'pycha4',
+#
+#         ]
+#
+#
+# class mathchapters(forms.ModelForm):
+#     class Meta:
+#         model =mathchapter
+#         fields = [
+#             'mathcha1',
+#             'mathcha2',
+#             'mathcha3',
+#             'mathcha4',
+#
+#         ]
+#
+# class algochapters(forms.ModelForm):
+#     class Meta:
+#         model =algochapter
+#         fields = [
+#             'algocha1',
+#             'algocha2',
+#             'algocha3',
+#             'algocha4',
+#
+#         ]
